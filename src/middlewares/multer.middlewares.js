3fbee4aa4dcb067d2c4607e11d0d235f026415fb
Aayush Chaudhary <aayushcyy@gmail.com>
2025-03-10 13:31:23 +0530

    changed the file upload type only to png, jpg, jpeg with the max size only of 5mb

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,6 +1,19 @@
 import multer from "multer";
 import dayjs from "dayjs";
 
+// File type validation
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (!allowedTypes.includes(file.mimetype)) {
+    return cb(
+      new Error("Invalid file type. Only JPEG and PNG are allowed."),
+      false
+    );
+  }
+  cb(null, true);
+};
+
+// Storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     console.log("Multer saving to ./public/temp");
@@ -8,11 +21,16 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     console.log("Multer file:", file);
-    const uniqueSuffix =
-      dayjs().format("DMMMYY") + "-" + dayjs().format("Hmmss");
-    cb(null, file.originalname + "-" + uniqueSuffix);
+    const uniqueSuffix = dayjs().format("DMMMYY-Hmmss");
+    cb(null, `${file.fieldname}-${uniqueSuffix}-${file.originalname}`);
   },
 });
 
-const upload = multer({ storage: storage });
+// Multer upload middleware
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  fileFilter: fileFilter,
+});
+
 export default upload;
